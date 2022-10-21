9196a500a3f8b27979a0772dd554c22cfd00cbf6
danielpark <danielpark1140@gmail.com>
2022-10-21 13:41:39 -0700

    move script to own class

---
+++ public/assets/script.js
@@ -0,0 +1,18 @@
+let interval;
+const eventDay = new Date('10/24/2022').getTime();
+const hour = 1000 * 60 * 60;
+const day = hour * 24;
+
+const countdown = setInterval(() => {
+let now = new Date().getTime();
+let distance = eventDay - now;
+let days = Math.floor(distance/day);
+let hours = Math.floor((distance % day) / hour);
+
+document.getElementById('countdown').innerHTML = `${days} days, ${hours} hours`;
+
+if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById('countdown').innerHTML = "how's she doing?"
+}
+});
